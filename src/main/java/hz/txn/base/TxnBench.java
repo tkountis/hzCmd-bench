fc7e05de736049849b8bb5ac203a566fb10253c1
danny-hazelcast <danny@hazelcast.com>
2016-05-12 16:10:01 +0300

    hzCmd-bench

---
+++ src/main/java/hz/txn/base/TxnBench.java
@@ -0,0 +1,32 @@
+package hz.txn.base;
+
+import base.BenchBase;
+import com.hazelcast.core.HazelcastInstance;
+import com.hazelcast.transaction.TransactionOptions;
+
+import static com.hazelcast.transaction.TransactionOptions.TransactionType.ONE_PHASE;
+import static com.hazelcast.transaction.TransactionOptions.TransactionType.TWO_PHASE;
+
+
+public abstract class TxnBench extends BenchBase {
+
+    protected HazelcastInstance hzInstance;
+    protected TransactionOptions txnOps;
+    public int phase=1;
+
+    public void init() {
+        super.init();
+
+        if(phase==1) {
+            txnOps =new TransactionOptions().setTransactionType(ONE_PHASE);
+        }else{
+            txnOps =new TransactionOptions().setTransactionType(TWO_PHASE);
+        }
+    }
+
+    public void cleanup() { }
+
+    public void setVendorObject(Object o) {
+        hzInstance = (HazelcastInstance)o;
+    }
+}

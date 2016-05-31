cfe0075366f4d0f220d71bce106e67c0737854f3
danny-hazelcast <danny@hazelcast.com>
2016-05-31 14:41:06 +0300

    hzCmd-bench

--- src/main/java/hz/ops/ClusterShutDown.java
+++ src/main/java/hz/ops/ClusterShutDown.java
@@ -5,7 +5,10 @@ import hz.ops.base.OpsBase;
 public class ClusterShutDown extends OpsBase {
 
     public void timeStep() throws Exception {
+
+        System.out.println(this.getClass().getName()+" before hzInstance.getCluster().shutdown();");
         hzInstance.getCluster().shutdown();
+        System.out.println(this.getClass().getName()+" after hzInstance.getCluster().shutdown();");
         running=false;
     }
 

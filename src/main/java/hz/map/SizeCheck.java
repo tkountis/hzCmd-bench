f8559d4fc65f478c5fe6e366afd685d8547ee7f7
danny-hazelcast <danny@hazelcast.com>
2016-07-08 23:21:13 +0300

    hzCmd-bench

--- src/main/java/hz/map/SizeCheck.java
+++ src/main/java/hz/map/SizeCheck.java
@@ -6,6 +6,7 @@ import hz.map.base.MapBench;
 
 public class SizeCheck extends MapBench {
 
+    private boolean running=true;
     public int size;
 
     public void timeStep() throws AssertionException {
@@ -13,9 +14,10 @@ public class SizeCheck extends MapBench {
         if(map.size()!=size){
             throw new AssertionException("map "+map.getName()+" size "+map.size()+" != expected "+size);
         }
+        running=false;
     }
 
-    public boolean isRunning() { return false; }
+    public boolean isRunning() { return running; }
 
     public boolean isSelfDetermined() {
         return true;

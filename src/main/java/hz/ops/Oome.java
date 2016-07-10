314900e7a51595b3396b698ba2178b36879fc3bc
danny-hazelcast <danny@hazelcast.com>
2016-07-10 23:29:21 +0300

    hzCmd-bench

--- src/main/java/hz/ops/Oome.java
+++ src/main/java/hz/ops/Oome.java
@@ -2,11 +2,12 @@ package hz.ops;
 
 import hz.ops.base.OpsBase;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class Oome extends OpsBase {
 
-    protected List<byte[]> leak;
+    protected List<byte[]> leak = new ArrayList<byte[]>();
     protected int size=10000;
 
     public void timeStep() throws Exception {

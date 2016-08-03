9ebd2eceb8f1dd4dc708c0bb64c67db451199dcf
danny-hazelcast <danny@hazelcast.com>
2016-08-03 16:28:12 +0300

    hzCmd-bench

---
+++ src/main/java/hz/map/PutAllStatic.java
@@ -0,0 +1,47 @@
+package hz.map;
+
+import hz.map.base.MapBench;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class PutAllStatic extends MapBench {
+
+    public int mapCount =2;
+    public int batchSize=100;
+    private List<Map> putAllMaps = new ArrayList<Map>();
+
+    public void init() {
+        super.init();
+
+        for (int i = 0; i< mapCount; i++){
+            Map m = new HashMap();
+            for(int batch=0; i<batchSize; batch++) {
+
+                int key = random.nextInt(keyDomain);
+                while(m.containsKey(key)){
+                    key = random.nextInt(keyDomain);
+                }
+
+                Object v = mapKeyToValue(key);
+                m.put(key, v);
+            }
+            putAllMaps.add(m);
+        }
+    }
+
+
+    public void timeStep() {
+        map.putAll(getMap());
+    }
+
+    private Map getMap(){
+        return putAllMaps.get(random.nextInt(mapCount));
+    }
+
+    public void cleanup() {
+        super.cleanup();
+    }
+}

a6f05d55771630020f3273e38340fa6274835fa7
danny-hazelcast <danny@hazelcast.com>
2016-08-16 12:20:36 +0300

    hzCmd-bench

--- src/main/java/hz/map/member/ValidateByteArray.java
+++ src/main/java/hz/map/member/ValidateByteArray.java
@@ -2,15 +2,23 @@ package hz.map.member;
 
 import global.AssertionException;
 
-public class Validate extends Validator {
+import java.util.Arrays;
+
+public class ValidateByteArray extends Validator {
 
 
     public void validate(int key, Object value) throws Exception {
 
         Object expectedValue = mapKeyToValue(key);
 
+        if (Arrays.equals( (byte[]) expectedValue, (byte[]) value)) {
+            throw new AssertionException("key "+key+" value "+value+" != "+expectedValue);
+        }
+
+        /*
         if( ! expectedValue.equals(value) ){
             throw new AssertionException("key "+key+" value "+value+" != "+expectedValue);
         }
+        */
     }
 }

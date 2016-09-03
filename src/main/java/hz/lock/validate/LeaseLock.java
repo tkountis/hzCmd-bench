a3d3193bcd740cfa8693ce5be48120fdbff7114c
danny-hazelcast <danny@hazelcast.com>
2016-09-03 11:05:29 +0300

    hzCmd-bench

--- src/main/java/hz/lock/validate/LeaseLock.java
+++ src/main/java/hz/lock/validate/LeaseLock.java
@@ -1,23 +1,38 @@
 package hz.lock.validate;
 
+import com.hazelcast.core.ILock;
 import hz.lock.validate.base.LockValidate;
+
+import java.util.concurrent.TimeUnit;
 
 public class LeaseLock extends LockValidate {
 
-    public long seconds=30;
-    private int incCount=0;
+    public long seconds=10;
+    private int[] increments;
+
+    public void init() throws Exception{
+        super.init();
+        increments = new int[count];
+
+        for (int i=0; i<count; i++) {
+            zeroMapIdx(i);
+        }
+    }
 
     public void timeStep() {
-        /*
+
+        int i = random.nextInt(count);
+        ILock lock = getLock(i);
+
         lock.lock(seconds, TimeUnit.SECONDS);
-        int val = lockedMap.get(name);
-        utils.Utils.sleep(5);
-        lockedMap.put(name, ++val);
-        incCount++;
-        */
+        incMapIdx(i);
+        increments[i]++;
     }
 
     public void postPhase() {
-        //totalInc.addAndGet(incCount);
+        for (int i=0; i<count; i++) {
+            System.out.println("increments["+i+"]="+increments[i]);
+            addIncrementFor(i, increments[i]);
+        }
     }
 }
--- src/main/java/hz/lock/validate/TryLock.java
+++ src/main/java/hz/lock/validate/TryLock.java
@@ -1,31 +1,43 @@
 package hz.lock.validate;
 
+import com.hazelcast.core.ILock;
 import hz.lock.validate.base.LockValidate;
+
+import java.util.concurrent.TimeUnit;
 
 public class TryLock extends LockValidate {
 
     public long milliseconds=50;
-    private int incCount=0;
+    private int[] increments;
+
+    public void init() throws Exception{
+        super.init();
+        increments = new int[count];
+
+        for (int i=0; i<count; i++) {
+            zeroMapIdx(i);
+        }
+    }
 
     public void timeStep() {
-
-        /*
         try {
+            int i = random.nextInt(count);
+            ILock lock = getLock(i);
             if( lock.tryLock(milliseconds, TimeUnit.MILLISECONDS) ){
                 try {
-                    int val = lockedMap.get(name);
-                    utils.Utils.sleep(5);
-                    lockedMap.put(name, ++val);
-                    incCount++;
+                    incMapIdx(i);
+                    increments[i]++;
                 }finally {
                     lock.unlock();
                 }
             }
         } catch (InterruptedException e) { }
-        */
     }
 
     public void postPhase() {
-        //totalInc.addAndGet(incCount);
+        for (int i=0; i<count; i++) {
+            System.out.println("increments["+i+"]="+increments[i]);
+            addIncrementFor(i, increments[i]);
+        }
     }
 }

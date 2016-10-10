a9090b3cdfd2e8da0392dc56c75eccdc7659910c
danny-hazelcast <danny@hazelcast.com>
2016-10-10 12:51:28 +0300

    hzCmd

---
+++ src/main/java/hz/list/base/MapBench.java
@@ -0,0 +1,26 @@
+package hz.map.base;
+
+import base.BenchBase;
+import com.hazelcast.core.HazelcastInstance;
+import com.hazelcast.core.IMap;
+
+
+public abstract class MapBench extends BenchBase {
+
+    protected HazelcastInstance hzInstance;
+    protected IMap map;
+
+    public void init() throws Exception{
+        super.init();
+        map = hzInstance.getMap(name);
+    }
+
+    public void setVendorObject(Object o) {
+        hzInstance = (HazelcastInstance)o;
+    }
+
+    @Override
+    public String toString() {
+        return super.toString() + " MapBench{" + "hzInstance=" + hzInstance + ", map=" + map + '}';
+    }
+}

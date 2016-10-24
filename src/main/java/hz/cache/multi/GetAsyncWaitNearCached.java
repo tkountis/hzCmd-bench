3a02b597025690a67804622ad066e4ed288d5910
danny-hazelcast <danny@hazelcast.com>
2016-10-24 17:51:58 +0300

    hzCmd-bench

---
+++ src/main/java/hz/cache/multi/GetAsyncWaitNearCached.java
@@ -0,0 +1,40 @@
+package hz.cache.multi;
+
+import com.hazelcast.cache.ICache;
+import com.hazelcast.core.ICompletableFuture;
+import hz.cache.base.MultiCacheBench;
+
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.TimeoutException;
+
+public class GetAsyncWaitNearCached extends MultiCacheBench {
+
+    public int timeout=0;
+    public double repeat=0.3;
+
+    ICache cache;
+    Object key;
+
+    public void init() throws Exception {
+        super.init();
+
+        cache = getCache();
+        key = randomKey();
+    }
+
+
+    public void timeStep() throws InterruptedException, ExecutionException {
+
+        if( !(random.nextDouble() < repeat) ){
+            cache = getCache();
+            key = randomKey();
+        }
+
+        ICompletableFuture f = cache.getAsync(key);
+        try {
+            f.get(timeout, TimeUnit.MILLISECONDS);
+        } catch (TimeoutException t) { }
+    }
+
+}

bfd6e6f5b96fe4151d10088fbea48eca47aea8e2
danny-hazelcast <danny@hazelcast.com>
2016-12-13 09:46:41 +0700

    Revert: hzCmd-bench

--- src/main/java/hz/map/aggregate/Count.java
+++ src/main/java/hz/map/aggregate/Count.java
@@ -7,14 +7,14 @@ import hz.map.base.MapBench;
 
 public class Count extends MapBench {
 
-    public long total;
+    public long count;
 
-    private Aggregator<Object, Long> count = Aggregators.count();
+    private Aggregator<Object, Long>  count_aggregator = Aggregators.count();
 
     public void timeStep() throws AssertionException {
-        Long aggregate = (Long) map.aggregate(count);
-        if(aggregate != total){
-            throw new AssertionException(aggregate +"!="+total);
+        Long aggregate = (Long) map.aggregate(count_aggregator);
+        if(aggregate != count){
+            throw new AssertionException(aggregate +"!="+count);
         }
     }
 
